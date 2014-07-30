03027563dd1cdad0a578f7e73e8076dd8fdf32ca
Shane Mendez <shanemendez223@gmail.com>
2014-07-29 22:40:21 -0500

    fixed typos

--- nagios-server.sh
+++ nagios-server.sh
@@ -6,16 +6,16 @@ REPASS="1"
 
 #Password loop
 while [ $PASS != $REPASS ]; do
-	read -s -p "Password: " PASS; echo
-	read -s -p "Retype: " REPASS; echo
+	read -s -p "Nagios Password: " PASS; echo
+	read -s -p "Retype Nagios Password: " REPASS; echo
 done
 
-udo debconf-set-selections <<< "postfix postfix/mailname string diggalabs.com"
+sudo debconf-set-selections <<< "postfix postfix/mailname string diggalabs.com"
 sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 sudo debconf-set-selections <<< "nagios3-cgi nagios3/adminpassword string $PASS"
 sudo debconf-set-selections <<< "nagios3-cgi nagios3/adminpassword-repeat string $PASS"
 
 
-apt-get install -y nagios3
+sudo apt-get install -y nagios3
 
 
