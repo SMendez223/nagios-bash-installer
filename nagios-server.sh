7978174aef84313c3695ef10689fe8e578bcf8ac
samendez <shanemendez223@gmail.com>
2016-09-14 16:28:39 -0500

    security improvement

--- nagios-server.sh
+++ nagios-server.sh
@@ -3,17 +3,17 @@
 PASS="0"
 REPASS="1"
 
-
+read -p "mailname: " NAME
 #Password loop
 while [ $PASS != $REPASS ]; do
 	read -s -p "Nagios Password: " PASS; echo
 	read -s -p "Retype Nagios Password: " REPASS; echo
 done
 
-sudo debconf-set-selections <<< "postfix postfix/mailname string diggalabs.com"
-sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-sudo debconf-set-selections <<< "nagios3-cgi nagios3/adminpassword string $PASS"
-sudo debconf-set-selections <<< "nagios3-cgi nagios3/adminpassword-repeat string $PASS"
+debconf-set-selections <<< "postfix postfix/mailname string $NAME"
+debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+debconf-set-selections <<< "nagios3-cgi nagios3/adminpassword string $PASS"
+debconf-set-selections <<< "nagios3-cgi nagios3/adminpassword-repeat string $PASS"
 
 
 sudo apt-get install -y nagios3
